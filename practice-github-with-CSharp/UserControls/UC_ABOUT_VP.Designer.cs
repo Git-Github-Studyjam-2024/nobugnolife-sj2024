aa181b26f047d67027c451418e9060ca791a265e
Văn Phát <vanphat15it@gmail.com>
2024-03-09 11:06:11 +0700

    add VP about page

--- practice-github-with-CSharp/UserControls/UC_ABOUT_VP.Designer.cs
+++ practice-github-with-CSharp/UserControls/UC_ABOUT_VP.Designer.cs
@@ -28,25 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::practice_github_with_CSharp.Properties.Resources.Phát;
+            this.pictureBox2.Location = new System.Drawing.Point(484, 25);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(304, 293);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(237, 221);
+            this.label1.Font = new System.Drawing.Font("Google Sans", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "vp";
+            this.label1.Size = new System.Drawing.Size(208, 39);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "LÊ VĂN PHÁT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(364, 78);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "🌱 I’m currently learning kotlin, c#/java\r\n👨‍💻 All of my projects are available" +
+    " at \r\nhttps://github.com/vanphatit";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Google Sans", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(303, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 30);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Facebook";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // UC_ABOUT_VP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
             this.Name = "UC_ABOUT_VP";
             this.Size = new System.Drawing.Size(819, 480);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,6 +97,9 @@
 
         #endregion
 
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
